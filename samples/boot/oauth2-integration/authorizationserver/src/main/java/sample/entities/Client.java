5cf2e3ae77ba50ac5bfca3a8ac20c1e881dd55b6
Mohammed Balhaddad <mbailhadadd@gmail.com>
2020-10-12 17:58:58 +0300

    - Delete the test files , hard to add test
    >> going further with the learning without the tests

--- samples/boot/oauth2-integration/authorizationserver/src/main/java/sample/entities/Client.java
+++ samples/boot/oauth2-integration/authorizationserver/src/main/java/sample/entities/Client.java
@@ -1,4 +1,35 @@
 package sample.entities;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Client {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String id;
+
+	private String clientId ;
+
+	public Client(){}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
 }
--- samples/boot/oauth2-integration/authorizationserver/src/test/java/sample/services/ClientServiceTest.java
+++
@@ -1,4 +0,0 @@
-package sample.services;
-
-public class ClientServiceTest {
-}
